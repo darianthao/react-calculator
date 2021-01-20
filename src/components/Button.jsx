d4e50abe1baa9d605a56750dffc6adf97f7b4765
unknown <justdarian01@gmail.com>
2021-01-19 20:08:19 -0600

    Implemented MathJS Eval Functions w/ clearButton Functions

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,9 +6,10 @@ const isOperator = val => {
     return !isNaN(val) || val === "." || val === "=";
 }
 
-const Button = props => <div className={`button-wrapper ${
+export const Button = props => <div className={`button-wrapper ${
     isOperator(props.children) ? null : "operator"
-}`}>{props.children}</div>;
+}`}
+onClick = {() => props.handleClick(props.children)}
+>{props.children}</div>;
 
 
-export default Button;
